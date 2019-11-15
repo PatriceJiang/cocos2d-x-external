e4fa43464cc7bb1015fd83b14f514548784dca33
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2019-11-15 17:20:56 +0800

    fix function error

--- src/luajit/src/msvcbuild.bat
+++ src/luajit/src/msvcbuild.bat
@@ -105,9 +105,9 @@ if exist luajit.exe.manifest^
   %LJMT% -manifest luajit.exe.manifest -outputresource:luajit.exe
 
 
-@rem @del *.obj *.manifest minilua.exe buildvm.exe
-@rem @del host\buildvm_arch.h
-@rem @del lj_bcdef.h lj_ffdef.h lj_libdef.h lj_recdef.h lj_folddef.h
+@del *.obj *.manifest minilua.exe buildvm.exe
+@del host\buildvm_arch.h
+@del lj_bcdef.h lj_ffdef.h lj_libdef.h lj_recdef.h lj_folddef.h
 @echo.
 @echo === Successfully built LuaJIT for Windows/%LJARCH% ===
 
