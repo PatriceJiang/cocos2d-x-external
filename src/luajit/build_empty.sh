954ee0e5b474939262a75cfca24827832a7d018a
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2019-11-18 16:06:53 +0800

    add files

---
+++ src/luajit/build_empty.sh
@@ -0,0 +1,14 @@
+
+C:/Users/pt-win/AppData/Local/Android/Sdk/cmake/3.10.2.4988404/bin/cmake  \
+--verbose=1 \
+-HC:/Github/cocos2d-x-external/src/luajit/src/empty \
+-BC:/Github/cocos2d-x-external/src/luajit/src/android-empty \
+-GNinja \
+-DANDROID_ABI=armeabi-v7a \
+-DANDROID_NDK=C:/Users/pt-win/AppData/Local/Android/Sdk/ndk/20.0.5594570 \
+-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:/Github/cocos2d-x-external/src/luajit/android-build/intermediates/cmake/arm7/debug/obj/armeabi-v7a \
+-DCMAKE_BUILD_TYPE=Debug \
+-DCMAKE_MAKE_PROGRAM=C:/Users/pt-win/AppData/Local/Android/Sdk/cmake/3.10.2.4988404/bin/ninja \
+-DCMAKE_TOOLCHAIN_FILE=C:/Users/pt-win/AppData/Local/Android/Sdk/ndk/20.0.5594570/build/cmake/android.toolchain.cmake \
+-DANDROID_NATIVE_API_LEVEL=23 \
+-DANDROID_TOOLCHAIN=clang \
