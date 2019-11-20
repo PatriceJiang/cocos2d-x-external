ca04764e7241aa8ad0afbefb4e600b06ebd6a271
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2019-11-20 17:33:31 +0800

    refactor 1

--- src/luajit/build_android.sh
+++ src/luajit/build_android.sh
@@ -6,6 +6,7 @@ ANDROID_SDK=C:/Users/pt-win/AppData/Local/Android/Sdk
 LUAJIT_ROOT=C:/Github/cocos2d-x-external/src/luajit
 NDK_ROOT=C:/Users/pt-win/AppData/Local/Android/Sdk/ndk/20.0.5594570
 CMAKE_DIR=$ANDROID_SDK/cmake/3.10.2.4988404/bin
+MinGW64=C:/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin
 
 $CMAKE_DIR/cmake  \
 --verbose=1 \
@@ -13,6 +14,8 @@ $CMAKE_DIR/cmake  \
 -B${LUAJIT_ROOT}/android-build \
 -GNinja \
 -DANDROID_ABI=arm64-v8a \
+-DLUAJIT_HOST_GENERATOR="MinGW Makefiles" \
+-DLUAJIT_MinGW64_PATH=$MinGW64 \
 -DANDROID_NDK=$NDK_ROOT \
 -DCMAKE_LIBRARY_OUTPUT_DIRECTORY=${LUAJIT_ROOT}/android-build/libs \
 -DCMAKE_BUILD_TYPE=Debug \
