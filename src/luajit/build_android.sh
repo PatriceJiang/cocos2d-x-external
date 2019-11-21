3b45da9fc5c3284532d7572af4d807dd9dd231ae
Arnold <40414978+PatriceJiang@users.noreply.github.com>
2019-11-21 10:55:44 +0800

    refactor 2

--- src/luajit/build_android.sh
+++ src/luajit/build_android.sh
@@ -8,12 +8,14 @@ NDK_ROOT=C:/Users/pt-win/AppData/Local/Android/Sdk/ndk/20.0.5594570
 CMAKE_DIR=$ANDROID_SDK/cmake/3.10.2.4988404/bin
 MinGW64=C:/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64/bin
 
+rm -rf ${LUAJIT_ROOT}/android-build
+
 $CMAKE_DIR/cmake  \
 --verbose=1 \
 -H${LUAJIT_ROOT} \
 -B${LUAJIT_ROOT}/android-build \
 -GNinja \
--DANDROID_ABI=x86 \
+-DANDROID_ABI=armeabi-v7a \
 -DLUAJIT_HOST_GENERATOR="MinGW Makefiles" \
 -DLUAJIT_MinGW64_PATH=$MinGW64 \
 -DANDROID_NDK=$NDK_ROOT \
