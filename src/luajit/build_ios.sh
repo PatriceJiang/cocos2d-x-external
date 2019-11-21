0fdefb8209e69ee1549fe2af1d5b57db75fde4e4
patricejiang <jiangzhan@chukong-inc.com>
2019-11-21 15:34:22 +0800

    save

--- src/luajit/build_ios.sh
+++ src/luajit/build_ios.sh
@@ -4,19 +4,23 @@ set -x
 
 LUAJIT_ROOT=/Users/cocos/Github/cocos2d-x-external/src/luajit
 
+rm -rf ${LUAJIT_ROOT}/ios-build 
 
 cmake  \
---verbose=1 \
 -H${LUAJIT_ROOT} \
 -B${LUAJIT_ROOT}/ios-build \
 -GXcode \
 -DCMAKE_SYSTEM_NAME=iOS \
--DCMAKE_OSX_ARCHITECTURES="armv7" \
+-DCMAKE_OSX_SYSROOT=iphoneos \
 -DCMAKE_C_FLAGS="-miphoneos-version-min=8.0" \
--DCMAKE_OSX_DEPLOYMENT_TARGET=9.3
+-DCMAKE_OSX_DEPLOYMENT_TARGET=9.3 \
+-DCMAKE_OSX_ARCHITECTURES="armv7" \
 
 
+
+#-DLUAJIT_IOS_ARCH="arm64" \
 ## armv7;armv7s;arm64;i386;x86_64
+#-DCMAKE_OSX_ARCHITECTURES="armv7" \
 
 
 
